13f7af4cbc4bbc512724b9bf1f04d6daf8ff51db
Joseph Camacho <camacho.joseph@gmail.com>
2023-10-09 05:54:23 -0400

    Fixed fc.sv

--- hdl/modules/fc.sv
+++ hdl/modules/fc.sv
@@ -17,17 +17,19 @@ module fc #(
 
     output logic fd_prop_done, // signal that the forward propagation is done
     output logic bk_prop_done, // signal that the backward propagation is done
-    output logic [N-1:0] control_out [0:NUM_LAYERS-1], // to get the saved weights
+    output logic [NUM_LAYERS-1:0][(N/3)-1:0] control_out, // to get the saved weights
 
     output logic [N-1:0] fout,
     output logic [N-1:0] bout
   );
 
   localparam NUM_LAYERS = clog3(N);
-  logic [N-1:0] fh [0:NUM_LAYERS]; // not NUM_LAYERS-1. Makes indexing easier.
-  logic [N-1:0] bh [0:NUM_LAYERS];
+  logic [NUM_LAYERS:0][N-1:0] fh ; // not NUM_LAYERS-1. Makes indexing easier.
+  logic [NUM_LAYERS:0][N-1:0] bh;
   assign fh[0] = fin;
   assign bh[NUM_LAYERS] = bin;
+  assign fout = fh[NUM_LAYERS];
+  assign bout = bh[0];
   
   logic [NUM_LAYERS:0] fd_prop_dones;
   assign fd_prop_dones[0] = fd_prop;
@@ -36,55 +38,55 @@ module fc #(
   assign bk_prop_done = bk_prop_dones[0];
   assign bk_prop_dones[NUM_LAYERS] = bk_prop;
 
-  genvar l, i;
+  genvar l, i, j;
   generate
-    for(l=0; l < NUM_LAYERS; l=l+1) begin
-        for(i=0; i<N; i=i+1)begin
-          if (z3(i,l))begin
-            unit3to3 unit(
-              .rst_in(rst_in),
-              .clk_in(clk_in),
-              .oscillator(oscillator),
-              .fd_prop(fd_prop),
-              .bk_prop(bk_prop),
-              .fin({
-                fh[l][i],
-                fh[l][p3(i,l)%N],
-                fh[l][n3(i,l)%N]
-              }),
-              .bin({
-                bh[l+1][i],
-                bh[l+1][p3(i,l)%N],
-                bh[l+1][n3(i,l)%N]
-              }),
-              .fout({
-                fh[l+1][i],
-                fh[l+1][p3(i,l)%N],
-                fh[l+1][n3(i,l)%N]
-              }),
-              .bout({
-                bh[l][i],
-                bh[l][p3(i,l)%N],
-                bh[l][n3(i,l)%N]
-              }),
-              .control_out(control_out[l][i])
-            );
-          end
+    for(l = 0; l < NUM_LAYERS; l = l + 1) begin
+      for(i = 0; i < N; i = i + 3**(l + 1)) begin
+        for(j = i; j < (i + 3**l); j = j + 1) begin
+          unit3to3 unit(
+            .rst_in(rst_in),
+            .clk_in(clk_in),
+            .oscillator(oscillator),
+            .fd_prop(fd_prop_dones[l]),
+            .bk_prop(bk_prop_dones[l+1]),
+            .fin({
+              fh[l][j%N],
+              fh[l][(j+(3**l))%N],
+              fh[l][(j+(2*3**l))%N]
+            }),
+            .bin({
+              bh[l+1][j%N],
+              bh[l+1][(j+(3**l))%N],
+              bh[l+1][(j+(2*3**l))%N]
+            }),
+            .fout({
+              fh[l+1][j%N],
+              fh[l+1][(j+(3**l))%N],
+              fh[l+1][(j+(2*3**l))%N]
+            }),
+            .bout({
+              bh[l][j%N],
+              bh[l][(j+(3**l))%N],
+              bh[l][(j+(2*3**l))%N]
+            }),
+            .control_out(control_out[l][j-i+(i / 3)])
+          );
         end
+      end
 
-        signal_delay fd_layer_done(
-          .clk_in(clk_in),
-          .rst_in(rst_in),
-          .in_signal(fd_prop_dones[l]),
-          .out_signal(fd_prop_dones[l + 1])
-        );
-        
-        signal_delay bk_layer_done(
-          .clk_in(clk_in),
-          .rst_in(rst_in),
-          .in_signal(bk_prop_dones[l + 1]),
-          .out_signal(bk_prop_dones[l])
-        );
+      signal_delay fd_layer_done(
+        .clk_in(clk_in),
+        .rst_in(rst_in),
+        .in_signal(fd_prop_dones[l]),
+        .out_signal(fd_prop_dones[l + 1])
+      );
+      
+      signal_delay bk_layer_done(
+        .clk_in(clk_in),
+        .rst_in(rst_in),
+        .in_signal(bk_prop_dones[l + 1]),
+        .out_signal(bk_prop_dones[l])
+      );
     end
   endgenerate
   
--- sim/fc_tb.sv
+++ sim/fc_tb.sv
@@ -11,7 +11,7 @@ module fc_tb();
   logic [8:0] bin;
   logic [8:0] fout;
   logic [8:0] bout;
-  logic [8:0] control_out [0:1];
+  logic [1:0][2:0] control_out;
 
   fc #(.N(9)) test_fc(
     .rst_in(rst_in),
@@ -27,7 +27,7 @@ module fc_tb();
   );
 
   always begin
-      #3;
+      #10;
       oscillator = !oscillator;
   end
 
@@ -39,8 +39,9 @@ module fc_tb();
   assign bin = ~fin ^ fout;
   
   initial begin
+    $dumpfile("fc.vcd"); //file to store value change dump (vcd)
+    $dumpvars(1,test_fc);
     $display("\n--------\nStarting Simulation!");
-
     fin = 9'b111000111;
     fd_prop = 0;
     bk_prop = 0;
@@ -53,6 +54,7 @@ module fc_tb();
     #1000
     $display("%b", fout);
     $display("%b", control_out[0]);
+    $display("%b", control_out[1]);
     $finish;
   end
 endmodule // fc_tb
