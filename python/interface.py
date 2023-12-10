756b372f0139a6337462f2fdab925c5ea641dc75
James Camacho <camacho.james@gmail.com>
2023-12-10 16:58:35 -0500

    Endian encoding fixed for addresses

---
+++ python/interface.py
@@ -0,0 +1,99 @@
+import os
+from bitarray import bitarray
+
+file_dir = os.path.dirname(os.path.abspath(__file__))
+instr_path = os.path.join(file_dir, "..", "hdl/control_unit/", "cpu.sv")
+
+
+def extract_params(path):
+    with open(path, "r") as f:
+        localparams = f.read().split(
+                r"/* ----- PYTHON PARSING BEGINS HERE ----- */"
+            )[1].split(
+                r"/* ----- PYTHON PARSING ENDS HERE ----- */"
+            )[0]
+        
+    params = dict()
+    for line in localparams.split("\n"):
+        try:
+            var, other, *_ = line.split("localparam ")[1].split("=")
+            var = var.strip()
+            val = int(other.split(";")[0].strip())
+            params[var] = val
+        except:
+            pass
+    return params
+
+instruction_set = extract_params(instr_path)
+
+io_bits = {
+    'send': '0',
+    'recv': '1'
+    }
+
+loc_bits = {
+      'data': '00',
+      'weight': '01',
+      'op': '10',
+      'inf': '11'
+    }
+
+loc_size = {
+    'data': 2048,
+    'weight': 3072,
+    'op': 8,
+    'inf': 1024
+    }
+
+def prepare_message(io, loc, addr, bits=None):
+    """
+    io - 'send' to the FPGA or 'recv' bits from it
+    loc - 'data', 'weight', or 'op' BRAMS, or you can read from the 'inf'erence.
+    addr - the address to read/write from, as an integer
+    """
+    addr_bits = bitarray(format(addr & 0xffff, 'b').zfill(16)) # last 16 bits of addr
+    msg_type = bitarray('00000') + io_bits[io] + loc_bits[loc]
+    bits = bitarray(bits or '')
+    msg = msg_type.tobytes() + addr_bits.tobytes()[::-1] + bits.tobytes()
+    # b = bitarray()
+    # b.frombytes(msg)
+    # print(msg)
+    # print(b)
+    return msg
+
+    msg = bitarray(bits or '') + addr_bits + msg_type
+    print(msg.tobytes())
+    return int(msg, 2).to_bytes(len(msg)//8, byteorder='little')
+
+    
+    return msg
+    
+def prepare_op(op):
+    if type(op) == str:
+        op = instruction_set[op]
+    return bin(op)[2:].zfill(8)
+
+def send(ser, loc, addr, bits):
+    msg = prepare_message('send', loc, addr, bits)
+    ser.write(msg)
+
+def recv(ser, loc, addr):
+    msg = prepare_message('recv', loc, addr)
+    ser.write(msg)
+    b = bitarray()
+    b.frombytes(ser.read(loc_size[loc] // 8))
+    return b
+if __name__ == "__main__":
+    import serial
+    import serial.tools.list_ports
+    import time
+
+    ports = serial.tools.list_ports.comports()
+    for port, desc, hwid in sorted(ports):
+            print("{}: {} [{}]".format(port, desc, hwid))
+    
+    ser = serial.Serial('/dev/ttyUSB1', 4_000_000, stopbits=serial.STOPBITS_TWO)
+    send(ser, 'op', 3, '00001010')
+    time.sleep(1.0)
+    print(recv(ser, 'op', 3))
+    ser.close()
--- python/test_cpu.py
+++ python/test_cpu.py
@@ -1,29 +1,46 @@
-import comms
+import interface
 import time
+from bitarray import bitarray
 
 import serial
-import serial.tools.list_ports
-
-ports = serial.tools.list_ports.comports()
-for port, desc, hwid in sorted(ports):
-        print("{}: {} [{}]".format(port, desc, hwid))
-
-ser = serial.Serial('/dev/ttyUSB1', 4_000_000)
+ser = serial.Serial('/dev/ttyUSB1', 4_000_000, stopbits=serial.STOPBITS_TWO) # helps it not lose bytes
 
 # Seed DATA
-comms.send(ser, 'data', 0, '01'*32*32)
-time.sleep(0.1)
-print(comms.recv(ser, 'data', 0))
+zero = bitarray('0'*1024)
 
-
-# Set INFERENCE to the OR of all DATA X's.
-ops = [7, 23, 10]#, 3, 0]
-for i, op in enumerate(ops):
+for i in range(512):
+    y = bitarray(format(i, 'b').zfill(1024))
     time.sleep(0.1)
-    comms.send(ser, 'op', i, bin(op)[2:].zfill(8))
+    interface.send(ser, 'data', i, y + zero)
+    time.sleep(0.1)
+    print(interface.recv(ser, 'data', i)[1016:1024])
+# x = bitarray('0011' * 256)
+# y = bitarray('0101' * 256)
+# # interface.send(ser, 'data', 3, zero*2)
+# interface.send(ser, 'data', 3, y + x)
 
-# Read INFERENCE
-time.sleep(1.0)
-print(comms.recv(ser, 'inf', 0))
+ops = [
+    "SET_XY_TO_VALUE_AT_D",
+    "SET_INFERENCE_TO_Y",
+    "D_INCREMENT",
+    ]
+for i, op in enumerate(ops):
+    bits = interface.prepare_op(op)
+    interface.send(ser, 'op', i, bits)
+
+inf = interface.recv(ser, 'inf', 0)
+print(inf)
+# assert inf == zero
+
+# bits = interface.prepare_op("D_INCREMENT")
+# interface.send(ser, 'op', 3, bits)
+
+time.sleep(0.5)
+inf = interface.recv(ser, 'inf', 0)
+print(inf)
+assert inf == x | y
+
+print("TEST PASSED WITH INSTRUCTIONS:\n")
+print(*ops, sep="\n")
 
 ser.close()
