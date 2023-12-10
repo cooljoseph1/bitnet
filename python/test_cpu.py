2630cc604188e400aeba08c33940b578bc674a14
James Camacho <camacho.james@gmail.com>
2023-12-09 22:11:57 -0500

    pyserial tester for cpu

--- python/test_cpu.py
+++ python/test_cpu.py
@@ -1,13 +1,27 @@
 import comms
+import time
 
-# Set INFERENCE to the OR of all DATA X's.
-ops = [7, 22, 15, 10, 3]
-for i, op in enumerate(ops):
-    comms.send('op', i, bin(op)[2:])
+import serial
+import serial.tools.list_ports
+
+ports = serial.tools.list_ports.comports()
+for port, desc, hwid in sorted(ports):
+        print("{}: {} [{}]".format(port, desc, hwid))
+
+ser = serial.Serial('/dev/ttyUSB1', 4_000_000)
 
 # Seed DATA
-comms.send('data', 0, '01'*1024)
+comms.send(ser, 'data', 0, '01'*1024)
+print(comms.recv(ser, 'data', 0))
+
+
+# Set INFERENCE to the OR of all DATA X's.
+ops = [3, 4, 7, 23, 10]#, 3, 0]
+for i, op in enumerate(ops):
+    comms.send(ser, 'op', i, bin(op)[2:].zfill(8))
 
 # Read INFERENCE
-for i in range(100):
-    print(recv('inf', 0))
+time.sleep(0.1)
+print(comms.recv(ser, 'inf', 0))
+
+ser.close()
